de6a2131dc1a98cbb1a0c4c29184ac631231e088
KirtanP01 <84253962+KirtanP01@users.noreply.github.com>
2025-09-16 17:19:45 -0400

    9/16/2025

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://api.purplecanvasstudio.com'
+  //apiUrl: 'https://api.purplecanvasstudio.com'
+  apiUrl: 'http://ec2-3-93-52-123.compute-1.amazonaws.com:5001'
 };
